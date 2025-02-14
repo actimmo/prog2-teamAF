60869dfd5e5b02ec92ad7b2f5b14d02aac24d6a5
stefanhFH <stefan.herzig@stud.fh-campuswien.ac.at>
2025-02-14 16:12:29 +0100

    movie.json and import function in movie class added

--- prog-2-teamAF/src/main/java/com/teamAF/app/models/Movie.java
+++ prog-2-teamAF/src/main/java/com/teamAF/app/models/Movie.java
@@ -1,7 +1,20 @@
 package com.teamAF.app.models;
 
+import com.teamAF.app.FhmdbApplication;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class Movie {
     private String title;
@@ -23,8 +36,22 @@ public class Movie {
 
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
-        // TODO add some dummy data here
+        try{
+            Path path = Paths.get(FhmdbApplication.class.getClassLoader().getResource("movies.json").toURI());
+            String jsonText = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+            JSONArray jsonArray = new JSONArray(jsonText);
 
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String title = jsonObject.getString("title");
+                String desc = jsonObject.getString("description");
+                Movie movie = new Movie(title, desc);
+                movies.add(movie);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return movies;
     }
 }
--- prog-2-teamAF/src/main/java/module-info.java
+++ prog-2-teamAF/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.teamAF.app {
     requires javafx.fxml;
 
     requires com.jfoenix;
+    requires org.json;
 
     opens com.teamAF.app to javafx.fxml;
     exports com.teamAF.app;
